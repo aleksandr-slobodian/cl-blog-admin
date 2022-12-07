a2477e0b5d0a01bb9779afc8034c99eb2095941b
Alex Slobodian <alexx.slobodyan@gmail.com>
2022-12-07 16:35:32 +0200

    add redux persist

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import counterReducer from "./state/counter/counterSlice";
 import themeReducer from "./state/theme/themeSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    theme: themeReducer,
-  },
+const rootPersistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["theme"],
+};
+
+const reducers = combineReducers({
+  counter: counterReducer,
+  theme: themeReducer,
 });
+
+const _persistedReducer = persistReducer(rootPersistConfig, reducers);
+
+export const store = configureStore({
+  reducer: _persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
