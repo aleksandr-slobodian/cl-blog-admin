b0dc2dfc6617de6f44e76886f7a5c1b1db315933
Alex Slobodian <alexx.slobodyan@gmail.com>
2023-02-14 15:00:49 +0200

    Improve drawers

--- src/state/drawers/drawersSlice.ts
+++ src/state/drawers/drawersSlice.ts
@@ -1,23 +1,29 @@
 import {
   createEntityAdapter,
   createSlice,
+  EntityState,
   PayloadAction,
 } from "@reduxjs/toolkit";
 import { RootState } from "../../store";
+import { KeyValue } from "../../types";
 
-type Drawer = { drawerId: string; isOpen?: boolean };
+type Drawer = { drawerId: string; isOpen?: boolean; data?: KeyValue };
 
 const drawersAdapter = createEntityAdapter<Drawer>({
   selectId: (drawer) => drawer.drawerId,
 });
+
+const getDrawerById = (state: EntityState<Drawer>, id: string) => {
+  const { selectById } = drawersAdapter.getSelectors();
+  return selectById(state, id);
+};
 
 export const drawerSlice = createSlice({
   name: "drawers",
   initialState: drawersAdapter.getInitialState(),
   reducers: {
     toggleDrawer: (state, action: PayloadAction<string>) => {
-      const { selectById } = drawersAdapter.getSelectors();
-      const drawer = selectById(state, action.payload);
+      const drawer = getDrawerById(state, action.payload);
       if (drawer) {
         drawersAdapter.updateOne(state, {
           id: drawer.drawerId,
@@ -30,10 +36,37 @@ export const drawerSlice = createSlice({
         });
       }
     },
+    openDrawer: (
+      state,
+      action: PayloadAction<{ name: string; data?: KeyValue }>
+    ) => {
+      const drawer = getDrawerById(state, action.payload.name);
+      if (drawer) {
+        drawersAdapter.updateOne(state, {
+          id: drawer.drawerId,
+          changes: { isOpen: true, data: action.payload?.data },
+        });
+      } else {
+        drawersAdapter.addOne(state, {
+          drawerId: action.payload.name,
+          isOpen: true,
+          data: action.payload?.data,
+        });
+      }
+    },
+    closeDrawer: (state, action: PayloadAction<{ name: string }>) => {
+      const drawer = getDrawerById(state, action.payload.name);
+      if (drawer) {
+        drawersAdapter.updateOne(state, {
+          id: drawer.drawerId,
+          changes: { isOpen: false },
+        });
+      }
+    },
   },
 });
 
-export const { toggleDrawer } = drawerSlice.actions;
+export const { toggleDrawer, openDrawer, closeDrawer } = drawerSlice.actions;
 
 export const selectDrawers = (state: RootState) => state.drawers;
 
